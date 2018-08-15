509abda24f53130373ae953cd52ce33055980667
tboychuk <tarass.boychuk@gmail.com>
2018-08-15 15:37:49 +0300

    Add new exercise FileReader

---
+++ file-reader/src/main/java/com/bobocode/FileReaders.java
@@ -0,0 +1,17 @@
+package com.bobocode;
+
+/**
+ * {@link FileReaders} privides an API that allow to read whole file into a {@link String} by file name.
+ */
+public class FileReaders {
+
+    /**
+     * Returns a {@link String} that contains whole text from the file specified by name.
+     *
+     * @param fileName a name of a text file
+     * @return string that holds whole file content
+     */
+    public static String readWholeFile(String fileName) {
+        throw new UnsupportedOperationException("It's your job to make it work!"); //todo
+    }
+}
