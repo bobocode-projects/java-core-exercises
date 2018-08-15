966991a35d4ababd3bc617205cdeecf5fc295bbd
tboychuk <tarass.boychuk@gmail.com>
2018-08-15 15:42:02 +0300

    Add dirty implementation

--- file-reader/src/main/java/com/bobocode/FileReaders.java
+++ file-reader/src/main/java/com/bobocode/FileReaders.java
@@ -1,5 +1,14 @@
 package com.bobocode;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 /**
  * {@link FileReaders} privides an API that allow to read whole file into a {@link String} by file name.
  */
@@ -11,7 +20,29 @@ public class FileReaders {
      * @param fileName a name of a text file
      * @return string that holds whole file content
      */
-    public static String readWholeFile(String fileName) {
-        throw new UnsupportedOperationException("It's your job to make it work!"); //todo
+    public static String readWholeFile(String fileName) { // todo: refactor to follow clean code rules and use java best practices
+        Path path = null;
+        try {
+            path = Paths.get(FileReaders.class.getClassLoader().getResource(fileName).toURI());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        List<String> l = new ArrayList<>();
+        Stream<String> stream = null;
+        try {
+            stream = Files.lines(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stream.forEach(l::add);
+        stream.close();
+        String str = "";
+        for (int i = 0; i < l.size(); i++) {
+            str += l.get(i);
+            if (i != l.size() - 1) {
+                str += "\n";
+            }
+        }
+        return str;
     }
 }
