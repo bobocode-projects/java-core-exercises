6b19455628af6ca5d2ae31b3b1a30391f555c9d1
luckyloker <serhii.s.bondarenko@gmail.com>
2018-10-26 19:36:50 +0300

    Completed exercise

--- file-reader/src/main/java/com/bobocode/FileReaders.java
+++ file-reader/src/main/java/com/bobocode/FileReaders.java
@@ -1,9 +1,15 @@
 package com.bobocode;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 /**
  * {@link FileReaders} privides an API that allow to read whole file into a {@link String} by file name.
  */
 public class FileReaders {
+
+    private static final String PATH = "/home/serhii/IdeaProjects/bobocode/java-core-exercises/file-reader/src/test/resources/";
 
     /**
      * Returns a {@link String} that contains whole text from the file specified by name.
@@ -12,6 +18,18 @@ public class FileReaders {
      * @return string that holds whole file content
      */
     public static String readWholeFile(String fileName) {
-        throw new UnsupportedOperationException("It's your job to make it work!"); //todo
+        StringBuilder readedString = new StringBuilder();
+        File file = new File(PATH + fileName);
+        try (FileInputStream fin = new FileInputStream(file)) {
+            int character;
+            while (fin.available() != 0) {
+                character = fin.read();
+                readedString.append((char) character);
+            }
+        }
+        catch (IOException  e) {
+            e.printStackTrace();
+        }
+        return readedString.toString();
     }
 }
