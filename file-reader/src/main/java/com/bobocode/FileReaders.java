67e8442506b5435cdcabec2dae8dbef4bdb1f85c
shtramak <shtramak@gmail.com>
2018-11-02 09:35:40 +0200

    add my FileReaders implementation

--- file-reader/src/main/java/com/bobocode/FileReaders.java
+++ file-reader/src/main/java/com/bobocode/FileReaders.java
@@ -1,5 +1,12 @@
 package com.bobocode;
 
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 /**
  * {@link FileReaders} privides an API that allow to read whole file into a {@link String} by file name.
  */
@@ -12,6 +19,25 @@ public class FileReaders {
      * @return string that holds whole file content
      */
     public static String readWholeFile(String fileName) {
-        throw new UnsupportedOperationException("It's your job to make it work!"); //todo
+        try {
+            Path path = getPathToResourseFileByFileName(fileName);
+            Stream<String> lines = Files.lines(path);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            lines.forEach(str -> stringBuilder.append(str).append("\n"));
+
+            int lastIndexOfSeparator = stringBuilder.lastIndexOf("\n");
+
+            return lastIndexOfSeparator > 0 ?
+                    stringBuilder.substring(0, lastIndexOfSeparator) :
+                    "";
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Path getPathToResourseFileByFileName(String fileName) throws URISyntaxException {
+        URL resource = FileReaders.class.getClassLoader().getResource(fileName);
+        return Paths.get(resource.toURI());
     }
 }
